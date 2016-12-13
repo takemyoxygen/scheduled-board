2cf0055743b5b94c4ba3764396539642f200e137
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-12-13 22:22:16 +0100

    Use trello api from node server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+const Trello = require("node-trello");
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const keys = JSON.parse(fs.readFileSync(__dirname + "/trello-keys.json", "utf8"));
+const trello = new Trello(keys.key, keys.token);
 
 const express = require('express');
 const app = express();
@@ -10,6 +15,21 @@ const router = express.Router();
 
 router.get('/', function (req, res) {
   res.json({message: 'Hello there'}); 
+});
+
+router.get('/boards', (req, res) => {
+  trello.get("/1/members/me/boards", function(err, data) {
+    if (err){
+      res.status(500).send(err);
+    }
+    else{
+      var boards = data
+        .filter(board => !board.closed)
+        .map(board => ({name: board.name, id: board.id}));
+
+      res.json(boards);
+    }
+  });
 });
 
 router.put('/save', (req, res) => {
