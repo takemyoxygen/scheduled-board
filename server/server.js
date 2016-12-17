46274d90e89e1398d3431ee69f269383e55ca5b2
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2016-12-17 22:03:49 +0100

    Use session

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const session = require('express-session')
 const Trello = require("node-trello");
 
-const hostname = '127.0.0.1';
 const port = 3000;
 
 const keys = JSON.parse(fs.readFileSync(__dirname + "/trello-keys.json", "utf8"));
@@ -14,18 +14,19 @@ const app = express();
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.json({message: 'Hello there'}); 
+  req.session.visited = (req.session.visited || 0) + 1;
+  res.json({ message: 'Hello there', visited: req.session.visited });
 });
 
 router.get('/boards', (req, res) => {
-  trello.get("/1/members/me/boards", function(err, data) {
-    if (err){
+  trello.get("/1/members/me/boards", function (err, data) {
+    if (err) {
       res.status(500).send(err);
     }
-    else{
+    else {
       var boards = data
         .filter(board => !board.closed)
-        .map(board => ({name: board.name, id: board.id}));
+        .map(board => ({ name: board.name, id: board.id }));
 
       res.json(boards);
     }
@@ -33,8 +34,16 @@ router.get('/boards', (req, res) => {
 });
 
 router.put('/save', (req, res) => {
-  res.json({processed: true, request: req.body});
+  res.json({ processed: true, request: req.body });
 });
+
+app.use(session(
+  {
+    cookie: { maxAge: 1000 * 60 * 60 },
+    secret: "this is the secret",
+    resave: false,
+    saveUninitialized: false
+  }));
 
 app.use(bodyParser.json());
 app.use("/api", router);
@@ -42,7 +51,7 @@ app.use("/api", router);
 module.exports = {
   start: () => {
     app.listen(port, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
+      console.log(`Server running at http://127.0.0.1:${port}/`);
     });
   }
 };
