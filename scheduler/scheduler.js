4a62670384358317866c15a34bc247c4479e94a3
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2017-01-08 20:08:59 +0100

    Adjust scheduler to just invoke corresponding action of the server API

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,33 +1,19 @@
 const request = require('request');
-const Trello = require('node-trello');
-const Promise = require('promise');
-
-function loadSchedules() {
-    return new Promise((resolve, reject) => {
-        request("http://127.0.0.1:3000/api/schedules/all", (err, resp, data) => {
-            if (err) reject(err);
-            else resolve(JSON.parse(data));
-        });
-    });
-}
-
-function createTask(token, list, text){
-    console.log(`Creating a card in list ${list} with text "${text}"`);
-    const trello = new Trello("app-key", token)
-    trello.post('/1/cards', {idList: list, name: text}, (err, data) => {
-        if (err) console.dir(err);
-        else console.log("Card has been created.");
-    });
-}
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
     run: () => {
-        loadSchedules()
-            .then(schedules => {
-                console.log(`Loaded ${schedules.length} schedules.`);
-                schedules
-                    .filter(_ => _.status === "active")
-                    .forEach(_ => createTask(_.token, _.listId, _.text));
+        const config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json")));
+        request.post({
+                url: config.host + "/api/schedules/create-cards",
+                headers: {
+                    "secret-code": config.secretAuthCode
+                }
+            },
+            (error, response, body) => {
+                if (error) console.error(error);
+                else console.log(`Created ${JSON.parse(body).count} card(s)`);
             });
     }
-}
+};
